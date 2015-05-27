868ac6acb081b13616acd04c468de68c10986bf0
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-05-27 22:43:20 +0200

    Added create forms

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,8 +1,27 @@
 class WebsitesController < ApplicationController
+  def new
+  	@website = Website.new
+  end
+
+  def create
+  	@website = Website.new(website_params)
+  	if @website.save
+  		redirect_to websites_path
+  	else
+  		render 'new'
+  	end
+  end
+
   def index
-  	@websites = Website.all
+  	@websites = Website.all.order('created_at DESC')
   end
 
   def show
+  end
+
+  private
+
+  def website_params
+  	params.require(:website).permit(:name, :url, :found)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'websites/index'
-
-  get 'websites/show'
-
-  get 'websitesindex/show'
+  resources :websites
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
